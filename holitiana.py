571f560c187a9c7a825c67f114ea54ba1da16b86
lolie <lolieholitiana@gmail.com>
2024-06-17 16:05:03 +0300

    initial commit

---
+++ holitiana.py
@@ -0,0 +1,3 @@
+print("lolie")
+lolie = "hhjhkhkhkhk"
+print(lolie)
---
+++ new.py
@@ -0,0 +1,30 @@
+print(8*4)
+print(40-30)
+print("lolie")
+lolio = "lolie vhj vguvguvugvu gvuvutg_gyihhuohnouhouhoilh ibhibihbiuohouh"
+
+
+
+
+
+print("lolie vhj vguvguvugvu gvuvutg_gyihhuohnouhouhoilh ibhibihbiuohouh")
+
+lolio5 = "jnhbhjjghvgvvk 6*33"
+print(lolio5)
+print (lolio5)
+
+print("lolie")
+print("lolie")
+print("lolie")
+print("lolie")
+print("bonjour""lolie")
+print("bon")
+print(188/6)
+print("hello"+"sister")
+print("cat".upper ())
+print ("tana" .upper())
+print (len("musique"))
+lottery = [2,6,4,10,7]
+print(lottery)
+lo = len(lottery)
+print(lo)
